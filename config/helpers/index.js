a560c7069126ee0334878271db2f35fc77b56c21
Taylor Marlow <metallicax4xyou@gmail.com>
2025-04-26 00:03:52 -0400

    Update index.js

--- config/helpers/index.js
+++ config/helpers/index.js
@@ -1,5 +1,9 @@
 // config/helpers/index.js
+// Exports only the active helpers
+
 module.exports = {
     Validators: require('./validators'),
-    PoolProcessor: require('./poolProcessor'),
+    // PoolProcessor: require('./poolProcessor'), // REMOVED - File deleted
+    // If poolLoader needs to be accessed via helpers index (optional):
+    // PoolLoader: require('./poolLoader'),
 };
