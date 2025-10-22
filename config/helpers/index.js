cda7ed947f71914ae4771ae2559167d9808ee21f
Taylor Marlow <metallicax4xyou@gmail.com>
2025-10-22 12:47:18 -0400

    Delete config/helpers directory

--- config/helpers/index.js
+++
@@ -1,9 +0,0 @@
-// config/helpers/index.js
-// Exports only the active helpers
-
-module.exports = {
-    Validators: require('./validators'),
-    // PoolProcessor: require('./poolProcessor'), // REMOVED - File deleted
-    // If poolLoader needs to be accessed via helpers index (optional):
-    // PoolLoader: require('./poolLoader'),
-};
